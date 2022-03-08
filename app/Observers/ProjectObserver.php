d9a0cc472af44ab09e3a67115b9ad65b30b2c648
mac <mac@macs-MacBook.local>
2022-03-09 01:45:26 +0800

    Create project observer class.

---
+++ app/Observers/ProjectObserver.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Project;
+/**
+ * 
+ */
+class ProjectObserver
+{
+	/**
+     * Handle the task "created" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function created(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the task "updated" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function updated(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the task "deleted" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function deleted(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the task "restored" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function restored(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the task "force deleted" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function forceDeleted(Project $project)
+    {
+        //
+    }
+}
